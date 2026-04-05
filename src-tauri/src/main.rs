5a650df6ced1da50b4a88aa0964c5971978478c4
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-04-05 06:50:27 -0400

    added Discord RPC

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-  tauri::Builder::default()
-    .plugin(tauri_plugin_fs::init())
-    .run(tauri::generate_context!())
-    .expect("error while running application");
+use discord_rich_presence::{activity, DiscordIpc, DiscordIpcClient};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = DiscordIpcClient::new("1490295823215366196");
+
+    client.connect()?;
+    client.set_activity(
+        activity::Activity::new(),
+    )?;
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
+        .run(tauri::generate_context!())
+        .expect("error while running application");
+
+    client.close()?;
+    Ok(())
 }
