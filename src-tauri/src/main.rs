5346b501db18e6e4dd6aa7206c7bef84b84a465e
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-04-05 08:56:35 -0400

    added opener plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,11 +7,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = DiscordIpcClient::new("1490295823215366196");
 
     client.connect()?;
-    client.set_activity(
-        activity::Activity::new(),
-    )?;
+    client.set_activity(activity::Activity::new())?;
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_fs::init())
         .run(tauri::generate_context!())
         .expect("error while running application");
