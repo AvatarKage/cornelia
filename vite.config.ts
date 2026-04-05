33dc53b258c8447c47e8b565437741c511f04511
ΛvatarKage (アバター影) <avatarkage.darkshadow@gmail.com>
2026-04-05 05:20:10 -0400

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,35 @@ import { defineConfig } from "vite";
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
-export default defineConfig(async () => ({
+export default defineConfig({
+  root: "src",
+  assetsInclude: ["**/*.toml"],
   clearScreen: false,
   server: {
     port: 32312,
     strictPort: true,
     host: host || false,
+
     watch: {
-      ignored: ["**/src-tauri/**", "**/dist/**"],
+      ignored: ["**/src-tauri/**", "**/dist/**", "**/build/**"],
     },
-  }
-}));
+  },
+  optimizeDeps: {
+    include: [
+      "@tauri-apps/api",
+      "@tauri-apps/api/path",
+      "@tauri-apps/plugin-fs",
+    ],
+  },
+  ssr: {
+    noExternal: [
+      "@tauri-apps/api",
+      "@tauri-apps/plugin-fs",
+    ],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+  },
+});
