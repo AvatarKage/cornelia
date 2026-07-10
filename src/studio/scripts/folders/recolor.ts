a11f38b9ca99438a07a2f3bdc3447583f2f515b6
AvatarKage <contact@avatarkage.com>
2026-07-10 00:56:44 -0400

    removed blank link

--- src/studio/scripts/folders/recolor.ts
+++ src/studio/scripts/folders/recolor.ts
@@ -100,7 +100,6 @@ async function recolor(
         overlayFill = isCustomIconColor
             ? adjustColor(iconColor, colorSaturation, colorBrightness, colorContrast)
             : adjustedBack;
-
     }
 
     let align: string = "middle";
