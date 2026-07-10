51012f6e880d836e3ac2d6844d045726a182d27e
AvatarKage <contact@avatarkage.com>
2026-07-10 03:50:10 -0400

    added Linux support and disabled generation

--- src/cornelia/scripts/folders/call_cornelia.py
+++ src/cornelia/scripts/folders/call_cornelia.py
@@ -1,32 +1,50 @@
 import os
+from pathlib import Path
 from rich import print
 
 from src.cornelia.scripts.config import config
 from src.cornelia.scripts.folders.generate_folder import generate_folder
-from src.cornelia.scripts.core.update_folder_icon import update_folder_icon
+from src.cornelia.scripts.core.update_folder_icon import update_folder_icon_linux
+
 
 async def call_cornelia(folder_path: str):
-    folder_name = os.path.basename(folder_path).lower()
+    folder = Path(folder_path)
+    folder_name = folder.name.lower()
 
-    icon_preset = {}
+    icon_path = None
+    icon_preset = None
 
     """
     ————————————————————————————————————————————————————————————————
     Match folder name with generation rules
     ———————————————————————————————————————————————————————————————— 
     """
+
     for key, value in config["rules"]["exact"].items():
         if key.lower() == folder_name:
             icon_preset = value
+            icon_path = value
             break
+
+    if icon_path is None:
+        if os.name == "posix":
+            update_folder_icon_linux(folder_path)
+        elif os.name == "nt":
+            update_folder_icon_windows(folder_path)
+
+        return None
+
+    if not Path(icon_path).exists():
+        raise FileNotFoundError(f"Icon not found: {icon_path}")
 
     """
     ————————————————————————————————————————————————————————————————
     Generate folder
-    ———————————————————————————————————————————————————————————————— 
+    ————————————————————————————————————————————————————————————————
     """
+
     if icon_preset:
-        result = await generate_folder(icon_preset)
+        """result = await generate_folder(icon_preset)
 
         if (
             not result
@@ -35,7 +53,10 @@ async def call_cornelia(folder_path: str):
             or not result["options"].get("variant")
             or not result.get("id")
         ):
-            print(f"󰜡 [red]Skipping folder, incomplete generation result: {folder_path}, {result} [/red]")
+            print(
+                f"󰜡 [red]Skipping folder, incomplete generation result: "
+                f"{folder_path}, {result} [/red]"
+            )
             return None
 
         icon_path = os.path.join(
@@ -43,8 +64,12 @@ async def call_cornelia(folder_path: str):
             result["options"]["style"],
             result["options"]["variant"],
             f"{result['id']}.ico"
-        )
+        )"""
 
-        update_folder_icon(folder_path, icon_path)
+        if os.name == "nt":
+            update_folder_icon_windows(folder_path, icon_path)
+
+        elif os.name == "posix":
+            update_folder_icon_linux(folder_path, icon_path)
 
     return None
